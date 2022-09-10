892ee0f300b7d9d5523357e979c7f6b87ba11bc8
Filip Weidler <filip.weidler18@imperial.ac.uk>
2022-09-11 00:16:43 +0200

    implementing pattern

--- life/life.py
+++ life/life.py
@@ -1,14 +1,17 @@
+"""Module that defines class for playing game of life."""
+
 import numpy as np
 from matplotlib import pyplot
 from scipy.signal import convolve2d
 
-glider = np.array([[0,1,0],[0,0,1],[1,1,1]])
-
+glider = np.array([
+    [0, 1, 0],
+    [0, 0, 1],
+    [1, 1, 1]])
 blinker = np.array([
-[0,0,0],
-[1,1,1],
-[0,0,0]])
-
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0]])
 glider_gun = np.array([
     [0, 0, 0, 0, 1, 1, 0, 0, 0],
     [0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -45,33 +48,44 @@ glider_gun = np.array([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 0, 0, 0, 0, 0]
-])
+    [0, 0, 1, 1, 0, 0, 0, 0, 0]])
+
 
 class Game:
-    def __init__(game,Size):
-        game.board = np.zeros((Size, Size))
-    
+
+    """Class defining the game of life."""
+    def __init__(game, size):
+        """Initialises size of board."""
+        game.board = np.zeros((size, size))
+
     def play(self):
+        """Start the game."""
         print("Playing life. Press ctrl + c to stop.")
         pyplot.ion()
         while True:
-            self.move(); self.show()
+            self.move()
+            self.show()
             pyplot.pause(0.0000005)
 
     def move(self):
+        """Defines what a move does."""
         STENCIL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        NeighbourCount = convolve2d(self.board, STENCIL, mode='same')
+        neighbour_count = convolve2d(self.board, STENCIL, mode='same')
 
         for i in range(self.board.shape[0]):
             for j in range(self.board.shape[1]):
-                self.board[i,j]=1 if (NeighbourCount[i, j]==3 or (NeighbourCount[i, j]==2 and self.board[i,j])) else 0
+                if (neighbour_count[i, j] == 3 or
+                        (neighbour_count[i, j] == 2 and self.board[i, j])):
+                    self.board = 1
+                else:
+                    self.board = 0
 
-    def __setitem__(self, key, value): 
+    def __setitem__(self, key, value):
+        """Sets the value of a square on the board."""
         self.board[key] = value
 
-
     def show(self):
-        pyplot.clf() 
+        """Prints out the game graphically."""
+        pyplot.clf()
         pyplot.matshow(self.board, fignum=0, cmap='binary')
         pyplot.show()
